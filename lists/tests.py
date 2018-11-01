ba3eb78e57bf38807706c4d84635734e733f470b
wj <jing8205@sina.com>
2018-11-01 10:34:26 +0800

    基本视图编写，返回一个最小网页

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -12,7 +14,13 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
 # class SmokeTest(TestCase):
 #     """docstring for SmokeTest"""
 
