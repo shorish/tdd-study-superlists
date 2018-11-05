e38550ebd31fe6dfc1af29554e8cd83910f400a2
wj <jing8205@sina.com>
2018-11-05 17:02:16 +0800

    重构前提交

--- lists/views.py
+++ lists/views.py
@@ -6,4 +6,9 @@ from django.http import HttpResponse
 
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
-    return render(request, 'home.html')
+    # if request.method == "POST":
+    #     return HttpResponse(request.POST['item_text'])
+    # return render(request, 'home.html')
+    return render(request, 'home.html',
+                  {'new_item_text': request.POST.get('item_text', ''),
+                   })
